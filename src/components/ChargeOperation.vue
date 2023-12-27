498e2b9a2b4ea7634c340fa8b69eca33bcb62c04
BrayanDev01 <dev01@grupocidade.com>
2023-12-27 16:03:38 -0400

    feat: refactor code

--- src/components/ChargeOperation.vue
+++ src/components/ChargeOperation.vue
@@ -142,12 +142,8 @@ export default{
             await axios.request(options).then((response) =>{
                 this.successToast(response.data.objectId)
                 this.closeModal()
-                // console.log(response)
-
             }).catch(error =>{
-
                 console.log(error)
-            
             })
         },
         openModal(){
@@ -201,6 +197,36 @@ export default{
         },
         onSelectedDate1(){
             this.$refs.meuCalendario1.overlayVisible = false
+        },
+        onSelectedDate2(){
+            this.$refs.meuCalendario2.overlayVisible = false
+        },
+        onSelectedDate3(){
+            this.$refs.meuCalendario3.overlayVisible = false
+        },
+        onSelectedDate4(){
+            this.$refs.meuCalendario4.overlayVisible = false
+        },
+        onSelectedDate5(){
+            this.$refs.meuCalendario5.overlayVisible = false
+        },
+        onSelectedDate6(){
+            this.$refs.meuCalendario6.overlayVisible = false
+        },
+        onSelectedDate7(){
+            this.$refs.meuCalendario7.overlayVisible = false
+        },
+        onSelectedDate8(){
+            this.$refs.meuCalendario8.overlayVisible = false
+        },
+        onSelectedDate9(){
+            this.$refs.meuCalendario9.overlayVisible = false
+        },
+        onSelectedDate10(){
+            this.$refs.meuCalendario10.overlayVisible = false
+        },
+        onSelectedDate11(){
+            this.$refs.meuCalendario11.overlayVisible = false
         }
     },
     created(){
@@ -235,7 +261,6 @@ export default{
             :pt="{
                 mask: {
                     style: 'backdrop-filter: blur(2px);'
-
                 },
                 root:{
                     style:'max-width: 100%; background-color: white'
@@ -333,7 +358,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="cargoStartOperation" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate2"
+                                            ref="meuCalendario2"
                                         />
                                         <label for="endOperation">Inicio da Operação</label>
                                     </span>
@@ -347,7 +373,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="cargoEndOperation" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate3"
+                                            ref="meuCalendario3"
                                         />
                                         <label for="endOperation">Fim da Operação</label>
                                     </span>
@@ -363,7 +390,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="cargoStartDraft" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate4"
+                                            ref="meuCalendario4"
                                         />
                                         <label for="endOperation">Inicio do Draft</label>
                                     </span>
@@ -377,7 +405,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="cargoEndDraft" 
-                                            dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate5"
+                                            ref="meuCalendario5"
                                         />
                                         <label for="endOperation">Fim do Draft</label>
                                     </span>
@@ -424,7 +453,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoDocking" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate6"
+                                            ref="meuCalendario6"
                                         />
                                         <label for="endOperation">Atracação</label>
                                     </span>
@@ -438,7 +468,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoUndocking" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate7"
+                                            ref="meuCalendario7"
                                         />
                                         <label for="endOperation">Desatracação</label>
                                     </span>
@@ -454,7 +485,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoStartOperation" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate8"
+                                            ref="meuCalendario8"
                                         />
                                         <label for="endOperation">Inicio da Operação</label>
                                     </span>
@@ -468,7 +500,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoEndOperation" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate9"
+                                            ref="meuCalendario9"
                                         />
                                         <label for="endOperation">Fim da Operação</label>
                                     </span>
@@ -484,7 +517,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoStartDraft" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate10"
+                                            ref="meuCalendario10"
                                         />
                                         <label for="endOperation">Inicio do Draft</label>
                                     </span>
@@ -498,7 +532,8 @@ export default{
                                             showTime 
                                             hourFormat="24" 
                                             v-model="uncargoEndDraft" 
-                                            @dateSelect="onSelectedDate"
+                                            @dateSelect="onSelectedDate11"
+                                            ref="meuCalendario11"
                                         />
                                         <label for="endOperation">Fim do Draft</label>
                                     </span>
